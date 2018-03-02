227ec2baf7a319901ec2b8669f2249d62bb6471e
htt12 <harrisontruong2@gmail.com>
2018-03-02 10:52:29 -0800

    Added click handlers to mole class

---
+++ main.js
@@ -0,0 +1,14 @@
+$('document').ready(initializeApp);
+
+
+function initializeApp(){
+    attachClickHandlers();
+}
+
+function attachClickHandlers(){
+    $('.mole').on('click', handleClickEvent);        
+}
+
+function handleClickEvent(){
+    console.log('handClickEvent was called.')
+}
