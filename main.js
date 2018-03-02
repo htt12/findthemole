ae0109ab7bae6c69f29c6d392639292b96e6ced9
Eeps <eeps30@gmail.com>
2018-03-02 10:53:26 -0800

    added moveRandomMole function, not complete

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
-document.ready(){
-    
+$(document).ready(initializeApp);
+
+function initializeApp(){
+
+}
+
+function moveRandomMole(){
+    $('.box1').css({"-webkit-transform":"translate(100px,100px)"});
 }
