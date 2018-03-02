fb6dccc0635d9aaf586999381e2d53d25c4767e5
htt12 <harrisontruong2@gmail.com>
2018-03-02 11:55:37 -0800

    Only one click counts functionality added

--- main.js
+++ main.js
@@ -3,18 +3,16 @@ $('document').ready(initializeApp);
 var scoreCount = 0;
 
 function initializeApp(){
-    attachClickHandlers();
     moveRandomMole();
 }
 
-function attachClickHandlers(){
-    $('.mole').on('click', handleClickEvent);  
+function attachClickHandlers(moleNumber){
+    $('.mole'+moleNumber).on('click', handleClickEvent);
 }
 
 function handleClickEvent(){
-    console.log('handClickEvent was called.')
-
-    if($(this).hasClass('mole')){
+    $('.mole').off('click');
+    if($(this).hasClass('mole') && clickCount<3){
         scoreCount++;
         $('.score-value').text(scoreCount);
     }
@@ -22,13 +20,15 @@ function handleClickEvent(){
 
 function moveRandomMole(){
     
-    var randomNum = Math.floor(Math.random() * 3);
+    var randomNum = Math.floor((Math.random() * 3)+1);
     
-    if(randomNum === 0){
+    if(randomNum === 1){
         $('.mole1').addClass('animate');
-    }else if(randomNum === 1){
-        $('.mole2').addClass('animate');
     }else if(randomNum === 2){
+        $('.mole2').addClass('animate');
+    }else if(randomNum === 3){
         $('.mole3').addClass('animate');
     }
+
+    attachClickHandlers(randomNum);
 }
